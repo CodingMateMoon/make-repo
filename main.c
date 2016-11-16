88b5718af6b34ce8d081562791e6b74cd500017c
JiHun Choi <gemini10244@gmail.com>
2016-11-16 14:45:59 +0900

    modified 'getopt'->'getopt_long'

--- main.c
+++ main.c
@@ -3,14 +3,29 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
 
 int main(int argc, char* argv[]) {
 	int n;
 	extern char* optarg;
 	extern int optind;
+	// long options 
+	static struct option long_options[] = {
+		{"print", no_argument, 0, 'p'},
+		{"source", required_argument, 0, 's'},
+		{"output", required_argument, 0, 'o'},
+		{"add", required_argument, 0, 'a'},
+		{"delete", required_argument, 0, 'd'},
+		{"lib", required_argument, 0, 'l'},
+		{"flag", required_argument, 0, 'f'},
+		{"clean", no_argument, 0, 'c'},
+		{"version", no_argument, 0, 'v'},
+		{0, 0, 0, 0}
+	};
+
 
 	// Get Options And Switch Process
-	while((n = getopt(argc, argv, "pa:d:o:l:f:cv")) != -1) {
+	while((n = getopt_long(argc, argv, "pa:d:o:l:f:cv", long_options, &optind)) != -1) {
 		switch(n) {
 			case 'p' : // print Makefile
 				break;
